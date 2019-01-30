7f4e6dddc56e51b11ed9f8d834a1fd2934caa93c
larynab <fsovreign@gmail.com>
2019-01-30 14:15:59 -0800

    class-beta

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,13 +1,64 @@
 
-const toggleMenu = () => {
+class Menuopen {
+  constructor(menuOpen) {
+    //this.menu = menuOpen.querySelector('.menu');
+    this.menuButton = menuOpen.querySelector('.menu-button'); 
+    this.menuopen = menuOpen.querySelector('.menu--open');
+
+    this.menuButton.addEventListener('click', () => this.toggleMenu());  
+  };
   // Toggle the "menu--open" class on your menu refence. 
+  toggleMenu() {
+    this.menuopen.classList.toggle('toggle-on');
+    //this.menu.classList.toggle('toggle-on');
+  };
+}; 
+
+
+class Article {
+ constructor(domElement) {
+   // assign this.domElement to the passed in domElement
+   this.domElement = domElement;
+   // create a reference to the ".expandButton" class.
+   this.expandButton = domElement.querySelectorAll(".expandButton");
+   // Using your expandButton reference, update the text on your expandButton to say "expand"
+   this.expandButton.innerHTML = "expand";
+
+   console.log(this.expandButton);
+   // Set a click handler on the expandButton reference, calling the expandArticle method.
+   this.expandButton.addEventListener('click', () => this.expandArticle());
+ }
+ expandArticle() {
+   // Using our reference to the domElement, toggle a class to expand or hide the article.
+   this.expandButton.classList.toggle('article-open');
+ }
 }
 
-// Start Here: Create a reference to the ".menu" class
-const menu;
-// create a reference to the ".menu-button" class
-const menuButton;
-// Using your menuButton reference, add a click handler that calls toggleMenu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Panel {
   // constructor
@@ -29,10 +80,11 @@ class Panel {
     this.panelBtnClose.classList.toggle('hide-btn');
     this.panelContent.classList.toggle('toggle-on');
   }
-}
+};
 
 const panels = document.querySelectorAll('.panel');
 panels.forEach( panel => new Panel(panel));
+
 
 
 // DOM panels -> loop (forEach) -> forEach will iterate over each DOM panel individually -> create a new Panel class -> creates a new panel object -> returned back to the dom

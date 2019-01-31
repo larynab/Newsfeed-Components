c6f554a7b724d5f58e12417e4fa3f72b62e7103c
larynab <fsovreign@gmail.com>
2019-01-30 18:24:09 -0800

    article-alpha

--- Article/Article.js
+++ Article/Article.js
@@ -11,25 +11,19 @@
 
 */
 
+
 class Article {
- constructor(domElement) {
-   this.expandButton = domElement.querySelector(".expandButton");
-   this.expandButton.innerHTML = "Open";
-   this.article = domElement.querySelector('.article');
-   
-
-   
-   // Set a click handler on the expandButton reference, calling the expandArticle method.
-   this.expandButton.addEventListener('click', () => this.expandArticle());
- }
- expandArticle() {
-   // Using our reference to the domElement, toggle a class to expand or hide the article.
-   
-   this.article.classList.toggle('article-open');
-
- }
-}
+  constructor(aElement) {
+    this.aElement = aElement;
+    this.articleButton = aElement.querySelector('.expandButton');
+    this.articleButton.innerHTML = "Expand";
+    this.articleButton.addEventListener('click', () =>this.expandArticle());
+  }
+  expandArticle() {
+    this.aElement.classList.toggle('article-open');
+  
+  }
+};
 
 const articles = document.querySelectorAll('.article');
 articles.forEach( article => new Article(article));
-

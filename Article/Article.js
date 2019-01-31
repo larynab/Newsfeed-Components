4a281b5cf1ab9f73a2fe9009dfa12c05747b3324
larynab <fsovreign@gmail.com>
2019-01-30 17:10:00 -0800

    menu-alpha

--- Article/Article.js
+++ Article/Article.js
@@ -2,23 +2,6 @@
 
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
-class Article {
-  constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
-  }
-
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-
-  }
-}
 
 /* START HERE: 
 
@@ -28,77 +11,25 @@ class Article {
 
 */
 
-let articles;
-
-
-/*
-class Menuopen {
-  constructor(menuOpen) {
-    this.menuOpen = menuOpen;
-    this.menu = menuOpen.querySelectorAll('.menu');
-    this.menuButton = menuOpen.querySelector('.menu-button'); 
-    this.menuopen = menuOpen.querySelector('.menu--open');
-    
-    console.log(this.menuButton);
-    this.menuButton.addEventListener('click', () => this.toggleMenu());  
-  };
-  
-  toggleMenu() {
-    this.menuopen.classList.toggle('toggle-on');
-    
-  };
-}; 
-
-const Menubutton = new Menuopen({});
-
-
-/*
 class Article {
  constructor(domElement) {
-  
-   this.domElement = domElement;
+   this.expandButton = domElement.querySelector(".expandButton");
+   this.expandButton.innerHTML = "Open";
+   this.article = domElement.querySelector('.article');
    
-   this.expandButton = domElement.querySelectorAll(".expandButton");
-   
-   this.expandButton.innerHTML = "expand";
 
-   console.log(this.expandButton);
+   
    // Set a click handler on the expandButton reference, calling the expandArticle method.
    this.expandButton.addEventListener('click', () => this.expandArticle());
  }
  expandArticle() {
    // Using our reference to the domElement, toggle a class to expand or hide the article.
-   this.expandButton.classList.toggle('article-open');
+   
+   this.article.classList.toggle('article-open');
+
  }
 }
-*/
 
-/*
-class Panel {
-  // constructor
-  constructor(panel) {
-    // panel is the individual DOM panel
-    this.panelButtons = panel.querySelector('.panel-buttons');
-    this.panelBtnOpen = panel.querySelector('.panel-btn-open');
-    this.panelBtnClose = panel.querySelector('.panel-btn-close');
-    this.panelContent = panel.querySelector('.panel-content');
-    
-    //this.panelButtons.addEventListener('click', this.togglePanel.bind(this));
-    this.panelButtons.addEventListener('click', () => this.togglePanel());
-    
-  }
-  // methods
-  togglePanel() {
-    console.log(this.panelBtnOpen);
-    this.panelBtnOpen.classList.toggle('hide-btn');
-    this.panelBtnClose.classList.toggle('hide-btn');
-    this.panelContent.classList.toggle('toggle-on');
-  }
-};
+const articles = document.querySelectorAll('.article');
+articles.forEach( article => new Article(article));
 
-const panels = document.querySelectorAll('.panel');
-panels.forEach( panel => new Panel(panel));
-*/
-
-
-// DOM panels -> loop (forEach) -> forEach will iterate over each DOM panel individually -> create a new Panel class -> creates a new panel object -> returned back to the dom
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,20 +1,17 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-const menuOpen = document.getElementsByClassName('menu--open');
-  if (menuOpen.style.display === "none") {
-    menuOpen.style.display = "block";
-  } else {
-    menuOpen.style.display = "none";
-  }
+
+menu.classList.toggle('menu--open');
+
 };
 
 
 
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelectorAll('.menu');
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', event => {
-  toggleMenu(event);
-});
+
+menuButton.addEventListener('click', toggleMenu);
+
